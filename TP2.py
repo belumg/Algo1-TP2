b8a4b0096acb181a4e378bf56dc4e73f465c055c
belumg <107223731+belumg@users.noreply.github.com>
2022-06-26 17:26:48 -0300

    Listar playlists de YT

--- TP2.py
+++ TP2.py
@@ -1,13 +1,30 @@
+# Para trabajar con Spotify:
+import tekore as tk
+from webbrowser import open as web_open
+
+# Para trabajar con YouTube:
+import os
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+import googleapiclient.errors
+
+# Módulos propios:
+import AUTENTICACION as auth
 import TP2_VISUAL as vis
+
+
+ID_CLIENTE: str = "ea4916f2e2d144a992b0f2d7bed6c25d"
+URI_REDIRECCION: str = "http://localhost:8888/callback"
+SCOPE = tk.scope.every
 
 def opciones(numeros_permitidos :list) -> int:
     """
     Pre: Recibe una lista con los posibles numeros que puede tomar nuestra opcion.
     Post: Devuelve un entero que sigue los limites marcados.
     """
-    opcion_correcta :bool = False
+    opcion_correcta: bool = False
     while not opcion_correcta:
-        opcion :str = input("Ingrese una opcion: ")
+        opcion: str = input("Ingrese una opcion: ")
         if opcion.isnumeric():
             if int(opcion) in numeros_permitidos:
                 opcion_correcta :bool = True
@@ -15,15 +32,114 @@ def opciones(numeros_permitidos :list) -> int:
         else: print("   Ingrese un numero.")
     return int(opcion)
 
+def guardar_nuevo_perfil(refresh_token, nombre_perfil) -> None:
+    datos_guardar: tuple = (ID_CLIENTE, None, URI_REDIRECCION, refresh_token)
+    tk.config_to_file("cuentas_spotify.txt", datos_guardar, nombre_perfil)
+    
+def nuevo_perfil_spotify(nombre_perfil) -> None:
+    """Permite que el usuario inicie sesion y de permisos y luego guarda sus datos en un archivo."""
+    credenciales: tk.RefreshingCredentials = tk.RefreshingCredentials(ID_CLIENTE, redirect_uri=URI_REDIRECCION)
+    url, verificador = credenciales.pkce_user_authorisation(SCOPE)
+    print(vis.INSTRUCCIONES)
+    input("Presione Enter para que se abra Spotify: ")
+    web_open(url)
+    print()
+    print("Aqui abajo debes ingresar la URL que copiaste:  ")
+    url: str = input("--->  ").strip()
+    codigo: str= tk.parse_code_from_url(url)
+    token_usuario: tk.RefreshingToken = credenciales.request_pkce_token(codigo, verificador)
+    refresh_token: str = token_usuario.refresh_token
+    guardar_nuevo_perfil(refresh_token, nombre_perfil)
+    print(vis.DATOS_GUARDADOS)
+
+def nuevo_perfil():
+    nombre: str = input("Ingrese el nombre del perfil: ")
+    vis.youtube_spotify()
+    opcion: int = opciones([1, 2])
+    if opcion == 1:
+        pass
+    else:
+        nuevo_perfil_spotify(nombre)
+    with open("nombres_perfiles.txt", "a") as f:
+        f.write(nombre+"\n")
+
+def perfiles_guardados() -> list:
+    """
+    Devuelve una lista con los nombres de los perfiles guardados que saca de un archivo,
+    si no encuentra ese archivo entonces devuelve una lista vacia.
+    """
+    nombres_perfiles: list = []
+    try:
+        archivo = open("nombres_perfiles.txt", "r")
+    except FileNotFoundError:
+        print(vis.NO_PERFILES)
+    else:
+        for linea in archivo:
+            nombres_perfiles.append(linea.rstrip())
+        archivo.close()
+    finally:
+        return nombres_perfiles
+
+def elegir_perfil() -> str:
+    nombres_perfiles: list = perfiles_guardados()
+    if nombres_perfiles:
+        vis.visual_lista_elementos(nombres_perfiles, "Perfiles Guardados", True)
+        numeros_permitidos: list = [x for x in range(1,len(nombres_perfiles)+1)]
+        opcion: int = opciones(numeros_permitidos)
+        return nombres_perfiles[opcion-1]
+    else:
+        return "no_eligio_perfil"
+
+def manejo_perfiles():
+    terminar: bool = False
+    eligio_perfil: bool = False
+    while not terminar:
+        vis.menu_perfiles(eligio_perfil)
+        opcion: int = opciones([1, 2, 3])
+        if opcion == 1:
+            perfil: str = elegir_perfil()
+            if perfil != "no_eligio_perfil":
+                eligio_perfil: bool = True
+        elif opcion == 2:
+            nuevo_perfil()
+        else:
+            terminar: bool = True
+    if eligio_perfil:
+        return perfil
+    else:
+        return "no_eligio_perfil"
+
+def listar_playlistsYT() -> list:
+    playlistsYT: list = []
+    request = auth.youtube.playlists().list(
+                    part="snippet,contentDetails",
+                    maxResults=50,
+                    mine=True
+                )
+    response = request.execute()
+    # Capturo los nombres de las playlists:
+    for elementos in response["items"]:
+        playlistsYT.append(elementos["snippet"]["title"])
+
+    return playlistsYT
+
 def main() -> None:
     vis.inicio()
-    terminar :bool = False
-    while not terminar:
-        print(vis.MENU)
-        opcion :int = opciones(range(1,9))
-        if opcion == 1:
-            pass
-        elif opcion == 8:
-            terminar :bool = True
-            
+    perfil: str = manejo_perfiles()
+    if perfil != "no_eligio_perfil":
+        datos_usuario: tuple = tk.config_from_file("cuentas_spotify.txt", perfil, True)
+        token = tk.refresh_pkce_token(datos_usuario[0], datos_usuario[3])
+        print(token)
+        terminar: bool = False
+        while not terminar:
+            vis.menu_opciones()
+            opcion: int = opciones([1, 2, 3, 4, 5, 6, 7, 8])
+            if opcion == 1:
+                lista: list = listar_playlistsYT()
+                vis.visual_lista_elementos(lista, "YouTube", False)
+            elif opcion == 2:
+                pass
+            elif opcion == 8:
+                terminar: bool = True
+        
 main()
--- TP2_VISUAL.py
+++ TP2_VISUAL.py
@@ -1,4 +1,4 @@
-CENTRADO :int = 60
+LONGITUD = 45
 
 LOGO_INICIO :str = """
     ╔════════════════════════════════════════════════════════════╗
@@ -19,11 +19,22 @@ LOGO_INICIO :str = """
 """
 
 def inicio() -> None:
+    """Imprime el logo del software."""
     print(LOGO_INICIO)
     input("         Presione Enter para continuar: ")
-    print() 
+    print()
 
-MENU :str = """
+def menu_perfiles(eligio_perfil:bool) -> None:
+    """
+    Pre: Recibe un bool.
+    Post: Imprime un menu con 3 opciones (perfil guardado, ingresar perfil, salir o volver al menu).
+    """
+    if eligio_perfil:
+        mensaje: str = "Ir al menu principal"
+    else:
+        mensaje: str = "Terminar el programa."
+
+    print(f"""
                 ██████████████████████████████
                 █▄─▀█▀─▄█▄─▄▄─█▄─▀█▄─▄█▄─██─▄█
                 ██─█▄█─███─▄█▀██─█▄▀─███─██─██
@@ -31,21 +42,131 @@ MENU :str = """
 
          ╔══════════════════════════════════════════╗
 
-          [1] Autenticar perfil en Youtube y Spotify
+          [1] Seleccionar perfil guardado.                        
+                                                        
+          [2] Ingresar perfil.
 
-          [2] Listar las playlist actuales
+          [3] {mensaje}
+
+         ╚══════════════════════════════════════════╝  
+    """)
+
+def youtube_spotify() -> None:
+    """Imprime un menu con 3 opciones (Youtube, Spotify, Volver al menu)."""
+    print(f"""
+    ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+            Elegir plataforma:        
+
+        [1] Youtube 
+
+        [2] Spotify
+
+        [3] Volver al menu
+
+    ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+    """)
+
+INSTRUCCIONES :str = """
+    ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+              [INSTRUCCIONES]
+
+     - Logearse (si es necesario).
+     - Aceptar los permisos.
+     - Copiar nueva URL.
+
+    ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+"""
+
+DATOS_GUARDADOS :str = """
+    ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+      [DATOS GUARDADOS EXITOSAMENTE]
+    ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+"""
+
+NO_PERFILES:str = """
+    ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+              NO ENCONTRAMOS
+          NINGUN PERFIL GUARDADO
+    ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+"""
+
+ERROR_URL :str = """
+    ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+             [URL INCORRECTA]
+
+      Posibles causas:
+        - Mal ingreso de datos.
+        - No acepto los permisos.
+        - Copio mal la URL. 
+
+    ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+"""
+
+def menu_opciones():
+    print("""
+                ██████████████████████████████
+                █▄─▀█▀─▄█▄─▄▄─█▄─▀█▄─▄█▄─██─▄█
+                ██─█▄█─███─▄█▀██─█▄▀─███─██─██
+                ▀▄▄▄▀▄▄▄▀▄▄▄▄▄▀▄▄▄▀▀▄▄▀▀▄▄▄▄▀▀
+
+         ╔══════════════════════════════════════════╗
           
-          [3] Exportar playlist elegida a CSV
+          [1] Listar playlist
 
-          [4] Crear nueva playlist
+          [2] Exportar playlist (CSV)
 
-          [5] Buscar nuevos elementos para playlist
+          [3] Crear nueva playlist
 
-          [6] Sincronizar playlist elegida
+          [4] Buscar canciones
 
-          [7] Nube de palabras de playlist elegida
+          [5] Sincronizar playlist
+
+          [6] Analizar playlist
+
+          [7] Cambiar de perfil
 
           [8] Salir
 
-         ╚══════════════════════════════════════════╝  
+         ╚══════════════════════════════════════════╝        
+    """)
+
+OPCION_NO_DISPONIBLE :str = """
+    ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+          [OPCION NO DISPONIBLE]
+
+       Primero debes autenticar un 
+       perfil en alguna plataforma
+
+    ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+"""
+
+def visual_lista_elementos(lista_mostrar:list, mensaje:str, enumerar:bool) -> None:
+    """
+    Pre: Recibe una lista no vacia, un mensaje que sera el titulo del menu
+         y un bool que nos dira si tenemos que enumerar los elementos o no.
+    Post: Imprime los elementos de la lista en un menu que tiene el mensaje ingresado.
+    """
+    maxima_long = len(sorted(lista_mostrar, key=len, reverse=True)[0])
+    if maxima_long < LONGITUD: maxima_long = LONGITUD
+    print(f"┏━{'━'*maxima_long}━┓")
+    print(f"{mensaje}".center(maxima_long))
+    print(f"┣━{'━'*maxima_long}━┫\n")
+    if enumerar:
+        for numero, dato in enumerate(lista_mostrar, 1):
+            print(f" [{numero}] {dato}\n")
+    else:
+        for dato in lista_mostrar:
+            print(f" - {dato}\n")
+    print(f"┗━{'━'*maxima_long}━┛")
+    if not enumerar:
+        input(" Presione Enter para volver al menu: ")
+
+NO_PLAYLIST :str = """
+    ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+
+        No se encontraron playlists   
+         guardadas en esta cuenta.
+
+    ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
 """
